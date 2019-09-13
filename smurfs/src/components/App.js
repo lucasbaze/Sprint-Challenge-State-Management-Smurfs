84eb147982f5daed56b41654dfb30311ce7d8853
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-13 12:35:54 -0500

    deleting a smurf now works... 1 stretch goal completed... hands hurt from typing so much :)... finished

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,9 +3,9 @@ import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 import { useStateValue } from '../state';
-import { getSmurfs } from '../reducers';
+import { getSmurfs, deleteSmurf } from '../reducers';
 
-import { Header, Table, Icon, Container } from 'semantic-ui-react';
+import { Header, Table, Icon, Container, Button } from 'semantic-ui-react';
 import SmurfForm from './Form';
 
 const App = () => {
@@ -20,7 +20,15 @@ const App = () => {
             <h1>SMURFS! 2.0 W/ Redux</h1>
             <div>Welcome to your state management version of Smurfs!</div>
             <div>Start inside of your `src/index.js` file!</div>
-            <SmurfForm />
+            <Container
+                style={{
+                    marginTop: 20,
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <SmurfForm />
+            </Container>
             <Container style={{ marginTop: 20 }}>
                 {!Array.isArray(smurfs) || !smurfs.length ? (
                     <Header as="h1" content="Loading" />
@@ -28,7 +36,7 @@ const App = () => {
                     <Table celled>
                         <Table.Header>
                             <Table.Row>
-                                <Table.HeaderCell colSpan="4">
+                                <Table.HeaderCell colSpan="5">
                                     Smurfs
                                 </Table.HeaderCell>
                             </Table.Row>
@@ -37,6 +45,9 @@ const App = () => {
                                 <Table.HeaderCell>Name</Table.HeaderCell>
                                 <Table.HeaderCell>Age</Table.HeaderCell>
                                 <Table.HeaderCell>Height</Table.HeaderCell>
+                                <Table.HeaderCell textAlign="right" wi>
+                                    Remove
+                                </Table.HeaderCell>
                             </Table.Row>
                         </Table.Header>
 
@@ -44,9 +55,28 @@ const App = () => {
                             {smurfs.map((smurf, index) => {
                                 let cells = [
                                     smurf.id,
-                                    smurf.name,
+                                    {
+                                        icon: 'user',
+                                        content: smurf.name,
+                                    },
                                     smurf.age,
                                     smurf.height,
+                                    {
+                                        width: 'one',
+                                        textAlign: 'right',
+                                        content: (
+                                            <Button
+                                                icon="delete"
+                                                color="red"
+                                                onClick={() =>
+                                                    deleteSmurf(
+                                                        smurf.id,
+                                                        dispatch
+                                                    )
+                                                }
+                                            />
+                                        ),
+                                    },
                                 ];
 
                                 return <Table.Row key={index} cells={cells} />;
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,4 +1,14 @@
+//
+//CONSTANTS
+//
+
 export const GET_SMURFS = 'smurfs/get_smurfs';
+export const DELETE_SMURF = 'smurfs/delete_smurf';
+export const POST_SMURF = 'smurfs/post_smurf';
+
+//
+//REDUCER
+//
 
 export default function reducer(state, action) {
     console.log('Reducer loop start');
@@ -9,10 +19,26 @@ export default function reducer(state, action) {
                 ...state,
                 smurfs: action.payload,
             };
+        case POST_SMURF:
+            console.log('Posting a new smurf');
+            return {
+                ...state,
+                smurfs: action.payload,
+            };
+        case DELETE_SMURF:
+            console.log('Setting deleted smurfs');
+            return {
+                ...state,
+                smurfs: action.payload,
+            };
         default:
             return state;
     }
 }
+
+//
+//ACTION CREATORS
+//
 
 export const getSmurfs = async dispatch => {
     let url = 'http://localhost:3333/smurfs';
@@ -40,7 +66,7 @@ export const getSmurfs = async dispatch => {
 export const postSmurf = async (smurf, dispatch) => {
     let { name, age, height } = smurf;
 
-    let response = await fetch('http://localhost:3333/smurfs', {
+    let smurfs = await fetch('http://localhost:3333/smurfs', {
         method: 'POST',
         mode: 'cors',
         cache: 'default',
@@ -51,7 +77,43 @@ export const postSmurf = async (smurf, dispatch) => {
         redirect: 'follow',
         referrer: 'client',
         body: JSON.stringify({ name, age, height }),
-    }).then(response => console.log(response.json()));
+    })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            return data;
+        });
 
-    getSmurfs(dispatch);
+    dispatch({
+        type: POST_SMURF,
+        payload: smurfs,
+    });
+};
+
+export const deleteSmurf = async (id, dispatch) => {
+    let url = `http://localhost:3333/smurfs/${id}`;
+
+    let smurfs = await fetch(url, {
+        method: 'DELETE',
+        mode: 'cors',
+        credentials: 'omit',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrer: 'no-referrer',
+    })
+        .then(response => {
+            console.log(response);
+            return response.json();
+        })
+        .then(data => {
+            return data;
+        });
+
+    dispatch({
+        type: DELETE_SMURF,
+        payload: smurfs,
+    });
 };
